17d3259ea489c38c640963b4ca1e1d2d678f6a2d
timofey <timofey13052005@gmail.com>
2025-08-01 18:45:44 +0300

    implemented service

--- src/main/java/controller/TaskController.java
+++ src/main/java/controller/TaskController.java
@@ -1,4 +1,28 @@
 package controller;
 
-public class TaskController {
+/*
+
+    Add, Update, and Delete tasks
+    Mark a task as in progress or done
+    List all tasks
+    List all tasks that are done
+    List all tasks that are not done
+    List all tasks that are in progress
+
+ */
+
+import java.util.List;
+
+import model.Task;
+
+public interface TaskController {
+    Task addTask(Task addedTask);
+    Task updateTask(Task updatedTask);
+    boolean deleteTask(Task deletedTask);
+    Task markTaskAsInProgress(Long id);
+    Task markTaskAsDone(Long id);
+    List<Task> getAllTasks();
+    List<Task> getDoneTasks();
+    List<Task> getNotDoneTasks();
+    List<Task> getInProgressTasks();
 }
